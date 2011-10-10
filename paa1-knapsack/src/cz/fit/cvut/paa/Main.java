adfcbc7d81755205c8b4808fdbde4fcfaa39c9e4
Watanabe <watanabe@medion.localdomain>
2011-10-10 16:16:24 +0200

    Solutions

--- paa1-knapsack/src/cz/fit/cvut/paa/Main.java
+++ paa1-knapsack/src/cz/fit/cvut/paa/Main.java
@@ -1,9 +1,12 @@
 package cz.fit.cvut.paa;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 import cz.cvut.felk.cig.jcop.algorithm.graphsearch.dfs.DepthFirstSearch;
 import cz.cvut.felk.cig.jcop.problem.knapsack.Knapsack;
@@ -15,13 +18,19 @@ public class Main {
 
 	private static final String[] DATA_FILES = { "data/knap_4.inst.dat",
 			"data/knap_10.inst.dat", "data/knap_15.inst.dat",
-			"data/knap_20.inst.dat"// , "data/knap_22.inst.dat",
-	// "data/knap_25.inst.dat", "data/knap_27.inst.dat",
-	// "data/knap_30.inst.dat", "data/knap_32.inst.dat",
-	// "data/knap_35.inst.dat", "data/knap_37.inst.dat",
-	// "data/knap_40.inst.dat"
+			"data/knap_20.inst.dat", "data/knap_22.inst.dat",
+			"data/knap_25.inst.dat", "data/knap_27.inst.dat",
+			"data/knap_30.inst.dat", "data/knap_32.inst.dat",
+			"data/knap_35.inst.dat", "data/knap_37.inst.dat",
+			"data/knap_40.inst.dat" };
 
-	};
+	private static final String[] SOLUTION_FILES = {
+			"solutions/knap_4.sol.dat", "solutions/knap_10.sol.dat",
+			"solutions/knap_15.sol.dat", "solutions/knap_20.sol.dat",
+			"solutions/knap_22.sol.dat", "solutions/knap_25.sol.dat",
+			"solutions/knap_27.sol.dat", "solutions/knap_30.sol.dat",
+			"solutions/knap_32.sol.dat", "solutions/knap_35.sol.dat",
+			"solutions/knap_37.sol.dat", "solutions/knap_40.sol.dat" };
 
 	private static Map<Integer, Double> relErrorsMap = new HashMap<Integer, Double>();
 	private static Map<Integer, Double> maxErrorsMap = new HashMap<Integer, Double>();
@@ -29,10 +38,18 @@ public class Main {
 	private static Map<Integer, Double> pwhAvgTimeMap = new HashMap<Integer, Double>();
 
 	public static void main(String[] args) throws IOException {
-		int startId = 9000;
-		for (String datafile : DATA_FILES) {
-			runExperiment(datafile, startId);
-			startId += 50;
+		int startIdx;
+		int endIdx;
+		// int startIdx = 0;
+		// int endIdx = 4;
+		// for (int i = startIdx; i < endIdx; i++) {
+		// runExperiment(DATA_FILES[i], 9000 + 50 * i);
+		// }
+
+		startIdx = 4;
+		endIdx = 12;
+		for (int i = startIdx; i < endIdx; i++) {
+			runExperiment(DATA_FILES[i], SOLUTION_FILES[i], 9000 + 50 * i);
 		}
 
 		System.out.println("Results:");
@@ -60,6 +77,67 @@ public class Main {
 			System.out.println(entry.getKey() + "    " + entry.getValue());
 		}
 
+	}
+
+	public static void runExperiment(String datafile, String solutionFile,
+			int startId) throws IOException {
+
+		double relErrorSum = 0.0;
+
+		double pwhTimeSum = 0.0;
+		double maxError = 0.0;
+		int dimension = (new Knapsack(new File(datafile))).getDimension();
+
+		Map<Integer, Integer> sol = getResults(new File(solutionFile));
+
+		for (int id = startId; id < startId + 50; id++) {
+			System.out.println("Processing: " + id);
+
+			Solver pwhSolver = new SimpleSolver(new PriceWeightHeuristic(),
+					new MyKnapsack(new File(datafile), Integer.toString(id)));
+			pwhSolver.run();
+			ResultEntry pwhEntry = pwhSolver.getResult().getResultEntries()
+					.get(0);
+
+			double pwhFitness = pwhEntry.getBestFitness();
+
+			pwhTimeSum += pwhEntry.getStartTimestamp().getCpuTimeSpent(
+					pwhEntry.getStopTimestamp());
+
+			double bruteFitness = sol.get(Integer.valueOf(id)).doubleValue();
+			double relError = ((bruteFitness - pwhFitness) / bruteFitness) * 100;
+			if (relError > maxError) {
+				maxError = relError;
+			}
+			relErrorSum += relError;
+
+		}
+
+		relErrorsMap.put(new Integer(dimension), relErrorSum / 50);
+		pwhAvgTimeMap.put(new Integer(dimension), pwhTimeSum / 50);
+		maxErrorsMap.put(new Integer(dimension), maxError);
+	}
+
+	public static Map<Integer, Integer> getResults(File f) throws IOException {
+		Map<Integer, Integer> solutions = new HashMap<Integer, Integer>();
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		String line;
+		Integer fitness;
+		while (true) {
+			line = br.readLine();
+			if (line == null) {
+				break;
+			}
+			StringTokenizer st = new StringTokenizer(line);
+
+			Integer id = Integer.valueOf(st.nextToken());
+			st.nextToken();
+			fitness = Integer.valueOf(st.nextToken());
+			solutions.put(id, fitness);
+		}
+
+		br.close();
+		return solutions;
 	}
 
 	public static void runExperiment(String datafile, int startId)
